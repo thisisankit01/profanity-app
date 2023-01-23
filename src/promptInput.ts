a1180ff8865f22c76e9b9836fa9a328d9e99d38b
Ankit Pandey <ankitechzz@gmail.com>
2023-01-23 22:02:36 +0530

    profanity app succesfully created

--- src/promptInput.ts
+++ src/promptInput.ts
@@ -8,6 +8,14 @@ const cussWordsHindi: string[] = [
   "chut",
 ];
 
+///////////////////////////////////add new cuss words here/////////////////////////////////////////////////////
+function addcussWordsHindi(newCuss: string) {
+  cussWordsHindi.push(newCuss)
+ };
+
+ addcussWordsHindi("lode"); //<-----for-ex---->
+
+ /////////////////////////////////////////prompt///////////////////////////////////////
 export const openAIPrompt: string = `
 These are the flag words in our api : ${cussWordsHindi.join(", ")}.
 Read the following messages and see if user is trying to use these words by changing the spelling a little bit or by using special characters or by using abbreviated forms. 
@@ -15,3 +23,7 @@ Answer :
 1) yes or no with a reason. 
 2) mention how many cuss words have been used .
 Messages : ${userInput}`
+
+
+
+
